76173bb2e2e1fa2e3760cf00e5588fec7c16433f
Dyeggo Silva <dyeggo0102@live.com>
2022-07-13 21:53:22 -0300

    Date select

--- backend/src/main/java/com/dytechnology/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/dytechnology/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,17 @@
 package com.dytechnology.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dytechnology.dsmeta.entities.sale;
 
 public interface SaleRepository extends JpaRepository<sale, Long> {
+	
+	@Query("SELECT obj FROM sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 
 }
